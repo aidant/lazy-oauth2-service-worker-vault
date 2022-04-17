5445edf85aa4570d435de42094884189566c5891
aidant <15520814+aidant@users.noreply.github.com>
2022-04-17 14:12:04 +1000

    Refactor

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Lazy Oauth2 Token Vault
+# Lazy OAuth 2.0 Token Vault
 
 <p align='center'>
-  Keep your Oauth2 tokens secure and up-to-date.
+  Keep your OAuth 2.0 tokens secure and up-to-date.
   <br>
   <a href='https://www.npmjs.com/package/@lazy/oauth2-token-vault'>
     <img src="https://img.shields.io/npm/v/@lazy/oauth2-token-vault?style=flat-square">
@@ -9,8 +9,8 @@
   <a href='https://bundlephobia.com/package/@lazy/oauth2-token-vault'>
     <img src="https://img.shields.io/bundlephobia/minzip/@lazy/oauth2-token-vault?label=minified%20%26%20gzipped&style=flat-square">
   </a>
-  <a href='https://github.com/aidant/oauth2-token-vault/actions/workflows/publish.yml'>
-    <img src="https://img.shields.io/github/workflow/status/aidant/oauth2-token-vault/Publish?style=flat-square">
+  <a href='https://github.com/aidant/lazy-oauth2-token-vault/actions/workflows/publish.yml'>
+    <img src="https://img.shields.io/github/workflow/status/aidant/lazy-oauth2-token-vault/Publish?style=flat-square">
   </a>
 </p>
 
@@ -20,11 +20,45 @@
 
 - [Example](#example)
 - [API](#api)
+  - [`modifyRequest`]
+  - [`modifyResponse`]
+  - [`fetchWithCredentials`]
+  - [`getAccessToken`]
+  - [`useRefreshToken`]
+  - [`fetchWithCredentialRefresh`]
+  - [`registerOauth2TokenInterceptor`]
 
 ## Example
 
+###### `application.ts`
+
 ```ts
-import {} from '@lazy/oauth2-token-vault'
+navigator.serviceWorker.register(
+  './service-worker.js?' +
+    new URLSearchParams({
+      oauth2_client_id: 'example-client-id',
+      oauth2_url_token: 'https://api.example.com/token',
+      oauth2_url_protected_resource_hostname: 'api.example.com',
+      oauth2_url_protected_resource_pathname: '/v1/*',
+    }),
+  { type: 'module' }
+)
+```
+
+###### `./service-worker.ts`
+
+```ts
+import { registerOauth2TokenInterceptor } from '@lazy/oauth2-token-vault'
+
+registerOauth2TokenInterceptor()
 ```
 
 ## API
+
+[`modifyrequest`]: #modifyrequest
+[`modifyresponse`]: #modifyresponse
+[`fetchwithcredentials`]: #fetchwithcredentials
+[`getaccesstoken`]: #getaccesstoken
+[`userefreshtoken`]: #userefreshtoken
+[`fetchwithcredentialrefresh`]: #fetchwithcredentialrefresh
+[`registeroauth2tokeninterceptor`]: #registeroauth2tokeninterceptor
--- src/oauth2-token-vault.ts
+++ src/oauth2-token-vault.ts
@@ -1,3 +1,17 @@
+declare var addEventListener: ServiceWorkerGlobalScope['addEventListener']
+declare var removeEventListener: ServiceWorkerGlobalScope['removeEventListener']
+
+const OAUTH2_CLIENT_ID = new URLSearchParams(location.search).get('oauth2_client_id') || ''
+const OAUTH2_TOKEN_URL = new URLSearchParams(location.search).get('oauth2_url_token') || ''
+// @ts-expect-error URLPattern does not yet have types.
+const OAUTH2_PROTECTED_RESOURCE_URL = new URLPattern(
+  Object.fromEntries(
+    [...new URLSearchParams(location.search).entries()]
+      .filter(([key]) => key.startsWith('oauth2_url_protected_resource_'))
+      .map(([key, value]) => [key.replace('oauth2_url_protected_resource_', ''), value])
+  )
+)
+
 const oauth2 = {
   access_token: '',
   token_type: '',
@@ -5,16 +19,24 @@ const oauth2 = {
   refresh_token: '',
 }
 
-const URL_OAUTH2_TOKEN = new URLSearchParams(location.search).get('url_oauth2_token')
-const URL_PROTECTED_RESOURCE = URLPattwen(
-  new URLSearchParams(location.search).get('url_protected_resource')
-)
+const isOauth2TokenURL = (url: string): boolean => OAUTH2_TOKEN_URL === url
+const isOauth2ProtectedResourceURL = (url: string): boolean =>
+  OAUTH2_PROTECTED_RESOURCE_URL.test(url)
 
-const isURLOauth2Token = (url: string): boolean => URL_OAUTH2_TOKEN === url
-const isURLProtectedResource = (url: string): boolean => URL_PROTECTED_RESOURCE.test(url)
+export const modifyRequest = (request: Request): Request => {
+  if (isOauth2ProtectedResourceURL(request.url) && oauth2.token_type && oauth2.access_token) {
+    const headers = new Headers(request.headers)
+    if (!headers.has('Authorization')) {
+      headers.set('Authorization', `${oauth2.token_type} ${oauth2.access_token}`)
+    }
+    return new Request(request, { headers })
+  }
 
-export const useAccessToken = async (response: Response): Promise<Response> => {
-  if (isURLOauth2Token(response.url) && response.status === 200) {
+  return request
+}
+
+export const modifyResponse = async (response: Response): Promise<Response> => {
+  if (isOauth2TokenURL(response.url) && response.status === 200) {
     const { access_token, token_type, expires_in, refresh_token, ...payload } =
       await response.json()
 
@@ -33,41 +55,50 @@ export const useAccessToken = async (response: Response): Promise<Response> => {
   return response
 }
 
-export const getAccessToken = async (): Promise<Response> => {
-  const response = await fetch(URL_OAUTH2_TOKEN, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      grant_type: 'refresh_token',
-      refresh_token: oauth2.refresh_token,
-    }),
-  })
-
-  return useAccessToken(response)
+export const fetchWithCredentials = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
+  const request = input instanceof Request ? input : new Request(input, init)
+  return fetch(modifyRequest(request)).then(modifyResponse)
 }
 
-export const setAccessToken = (request: Request): void => {
-  if (isURLProtectedResource(request.url) && oauth2.token_type && oauth2.access_token) {
-    request.headers.set('Authorization', `${oauth2.token_type} ${oauth2.access_token}`)
-  }
-}
+export const getAccessToken = async (): Promise<Response> =>
+  fetchWithCredentials(
+    new Request(OAUTH2_TOKEN_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        client_id: OAUTH2_CLIENT_ID,
+        grant_type: 'refresh_token',
+        refresh_token: oauth2.refresh_token,
+      }),
+    })
+  )
 
 export const useRefreshToken = async (request: Request, response: Response): Promise<Response> => {
-  if (isURLProtectedResource(response.url) && response.status === 401 && oauth2.refresh_token) {
+  if (
+    isOauth2ProtectedResourceURL(response.url) &&
+    response.status === 401 &&
+    oauth2.refresh_token
+  ) {
     await getAccessToken()
-    setAccessToken(request)
-    return fetch(request).then(useAccessToken)
+    return fetchWithCredentials(request)
   }
 
   return response
 }
 
+export const fetchWithCredentialRefresh = (
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<Response> => {
+  const request = input instanceof Request ? input : new Request(input, init)
+  return fetchWithCredentials(request).then((response) => useRefreshToken(request, response))
+}
+
 export const registerOauth2TokenInterceptor = (): (() => void) => {
   const listener = (event: FetchEvent) => {
-    setAccessToken(event.request)
-    event.respondWith(fetch(event.request).then(useAccessToken))
+    event.respondWith(fetchWithCredentialRefresh(event.request))
   }
 
   addEventListener('fetch', listener)
